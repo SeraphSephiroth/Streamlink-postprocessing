c5b349f8b43b0dbd1650f2018e47f87daa5c1ff7
SeraphSephiroth <savior.sephiroth@hotmail.co.nz>
2021-04-10 19:21:54 +1200

    Update postprocess.ps1

--- postprocess.ps1
+++ postprocess.ps1
@@ -21,9 +21,9 @@ else {
 }
 ffmpeg -v debug -i ..\Recordings\$channel\stream.ts -i ..\Recordings\$channel\stream.jpg -map 1 -map 0 -c copy -disposition:0 attached_pic "..\ProcessedStreams\$channel\$($filename + ".mp4")"
 $remove = Read-Host -Prompt 'Do you want to remove the original recording, stream.jpg, and stream.description (Y/N)? Be sure that post processing is complete and that your output has no errors'
-if ($remove -eq 'y' -or 'Y' -or 'Yes' -or 'yes'){
+if (($remove -eq 'y') -or ($remove -eq 'Y') -or ($remove -eq 'yes') -or ($remove -eq 'Yes')){
 		Remove-Item ..\Recordings\$channel\stream.jpg
 		Remove-Item ..\Recordings\$channel\stream.description
-		Remove-Item ..\Recordings\$channel\stream.mp4
+		Remove-Item ..\Recordings\$channel\stream.ts
 		Remove-Item ..\Recordings\$channel\
 }
